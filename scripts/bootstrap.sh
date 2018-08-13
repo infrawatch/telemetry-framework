85de8702658799d683d2deb95fc8c352b8d82a12
Leif <lmadsen@redhat.com>
2018-08-13 11:28:51 -0400

    Update scripts to use a working directory

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,30 +2,40 @@
 
 OPENSHIFT_GIT_REPO="https://github.com/openshift/openshift-ansible"
 OPENSHIFT_BRANCH="openshift-ansible-3.9.39-1"
+_TOPDIR=`pwd`
 
-echo "-- Clone OpenShift ${OPENSHIFT_BRANCH}"
-git clone --depth 1 --branch ${OPENSHIFT_BRANCH} ${OPENSHIFT_GIT_REPO} > /dev/null 2>&1
+echo "-- Create working directory"
+if [ ! -d working ]; then
+    mkdir working
+fi
 
-echo "-- Overlay telemetry-framework on openshift-ansible"
-pushd openshift-ansible
-    echo "  -- apply components.yml patch"
-    patch -p1 < ../patches/components.yml.patch > /dev/null 2>&1
+pushd working
 
-    echo "  -- link playbooks"
-    pushd playbooks
-    cp -r ../../playbooks/* ./
-    popd
+    echo "-- Clone OpenShift ${OPENSHIFT_BRANCH}"
+    git clone --depth 1 --branch ${OPENSHIFT_BRANCH} ${OPENSHIFT_GIT_REPO} > /dev/null 2>&1
 
-    echo "  -- link roles"
-    pushd roles
-    cp -r ../../roles/* ./
-    popd
+    echo "-- Overlay telemetry-framework on openshift-ansible"
+    pushd openshift-ansible
+        echo "  -- apply components.yml patch"
+        patch -p1 < $_TOPDIR/patches/components.yml.patch > /dev/null 2>&1
 
-    # ignore symlinks we're adding
-    cat > .gitignore <<EOF
+        echo "  -- link playbooks"
+        pushd playbooks
+        cp -r $_TOPDIR/playbooks/* ./
+        popd
+
+        echo "  -- link roles"
+        pushd roles
+        cp -r $_TOPDIR/roles/* ./
+        popd
+
+        # ignore symlinks we're adding
+        cat > .gitignore <<EOF
 playbooks/sa-telemetry
 roles/sa_telemetry*
 EOF
+    popd
+
 popd
 
 echo "-- Done."
