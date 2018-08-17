dc2171e5fb80008a259d8d34032c9b9653ebe908
Leif <lmadsen@redhat.com>
2018-08-17 14:52:29 -0400

    Add base-infra-bootstrap to the bootstrap.sh script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+BASE_INFRA_BOOTSTRAP_GIT_REPO="https://github.com/redhat-nfvpe/base-infra-bootstrap"
 OPENSHIFT_GIT_REPO="https://github.com/openshift/openshift-ansible"
 OPENSHIFT_BRANCH="openshift-ansible-3.9.39-1"
 _TOPDIR=`pwd`
@@ -10,6 +11,12 @@ if [ ! -d working ]; then
 fi
 
 pushd working
+
+    echo "-- Clone base-infra-bootstrap"
+    git clone --depth 1 --branch master ${BASE_INFRA_BOOTSTRAP_GIT_REPO} > /dev/null 2>&1
+    pushd base-infra-bootstrap
+        ansible-galaxy install -r requirements.yml
+    popd
 
     echo "-- Clone OpenShift ${OPENSHIFT_BRANCH}"
     git clone --depth 1 --branch ${OPENSHIFT_BRANCH} ${OPENSHIFT_GIT_REPO} > /dev/null 2>&1
