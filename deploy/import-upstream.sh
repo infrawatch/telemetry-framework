8094bbcc17fab38114a526bdeeaf888070d9652c
Leif <lmadsen@redhat.com>
2019-06-07 16:44:37 -0400

    Start moving towards use of ImageStreams
    I'm working on migrating to the use of ImageStreams so that spec files
    for upstream vs downstream are more consistent and easier to deal with
    between environments. It should also allow for offline installs to be
    easier to work with.
    
    Unfortunately the AMQ Interconnect Operator doesn't seem to like the
    use of ImageStreams right now, so that particular manifest still
    points at a direct link to the downstream image for now. An issue has
    been opened with the Messaging team to address this in the future.
    
    For now, all changes present allow for the instantiation of SAF using
    all components from Red Hat Container Catalog.

---
+++ deploy/import-upstream.sh
@@ -0,0 +1,2 @@
+oc import-image coreos/prometheus-operator:v0.25.0 --from=quay.io/coreos/prometheus-operator:v0.25.0 --confirm
+oc import-image coreos/configmap-reload:v0.0.1 --from=quay.io/coreos/configmap-reload:v0.0.1 --confirm
