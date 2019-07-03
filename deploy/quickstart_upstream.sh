413e1f25054b92c6f041b9a4b9ef41c08b7ccd33
Paul Leimer <pleimer@localhost.localdomain>
2019-07-03 16:08:23 -0400

    Added quickstart script for deploying upstream

---
+++ deploy/quickstart_upstream.sh
@@ -0,0 +1,27 @@
+ minishift start
+oc login -u system:admin
+oc new-project sa-telemetry
+openssl req -new -x509 -batch -nodes -days 11000 \
+        -subj "/O=io.interconnectedcloud/CN=qdr-white.sa-telemetry.svc.cluster.local" \
+        -out qdr-server-certs/tls.crt \
+        -keyout qdr-server-certs/tls.key
+oc create secret tls qdr-white-cert --cert=qdr-server-certs/tls.crt --key=qdr-server-certs/tls.key
+
+ansible-playbook \
+    -e "registry_path=$(oc registry info)" \
+    -e "imagestream_namespace=$(oc project --short)" \
+    deploy_builder.yml
+
+# need to patch a node in order to allow the current version of the SGO to deploy a SG
+oc patch node localhost -p '{"metadata":{"labels":{"application": "sa-telemetry", "node": "white"}}}'
+
+# import downstream container images
+./import-upstream.sh
+
+# deploy the environment (requires interaction)
+./deploy.sh
+watch -n5 oc get pods
+
+# teardown the environment when done (requires interaction)
+./deploy.sh DELETE
+watch -n10 oc get all
