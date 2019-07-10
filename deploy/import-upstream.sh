9f1d9c6a4ef847bb0d34e38b272c97c2d21bc859
Leif <lmadsen@redhat.com>
2019-07-10 15:32:44 -0400

    Migrate to OpenShift v3.11 container images for upstream
    Instead of using the CoreOS (source) container images, migrate the import-upstream.sh to
    using the v3.11 container images from the OpenShift organization. Using the OpenShift
    organization should better align our upstream and downstream deployments.

--- deploy/import-upstream.sh
+++ deploy/import-upstream.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-oc import-image prometheus:v2.11.104 --from=quay.io/prometheus/prometheus --confirm
-oc import-image prometheus-operator:latest --from=quay.io/coreos/prometheus-operator:v0.27.0 --confirm
-oc import-image prometheus-configmap-reloader:latest --from=quay.io/coreos/configmap-reload --confirm
-oc import-image prometheus-config-reloader:latest --from=quay.io/coreos/prometheus-config-reloader:v0.30.1 --confirm
-oc import-image prometheus-alertmanager:v0.15.0 --from=quay.io/prometheus/alertmanager --confirm
+oc import-image prometheus:v2.11 --from=quay.io/openshift/origin-prometheus:v3.11 --confirm
+oc import-image prometheus-operator:latest --from=quay.io/openshift/origin-prometheus-operator:v3.11 --confirm
+oc import-image prometheus-configmap-reloader:latest --from=quay.io/openshift/origin-configmap-reload:v3.11 --confirm
+oc import-image prometheus-config-reloader:latest --from=quay.io/openshift/origin-prometheus-config-reloader:v3.11 --confirm
+oc import-image prometheus-alertmanager:v0.15.0 --from=quay.io/openshift/origin-prometheus-alertmanager:v3.11 --confirm
 
 oc import-image amq-interconnect:1.4-6 --from=quay.io/interconnectedcloud/qdrouterd:1.7.0 --confirm
-oc import-image amq-interconnect-operator --from=quay.io/interconnectedcloud/qdr-operator:1.0.0-beta2 --confirm
+oc import-image amq-interconnect-operator:latest --from=quay.io/interconnectedcloud/qdr-operator:1.0.0-beta2 --confirm
 
-oc import-image smart-gateway --from=quay.io/redhat-service-assurance/smart-gateway --confirm
-oc import-image smart-gateway-operator --from=quay.io/redhat-service-assurance/smart-gateway-operator --confirm
+oc import-image smart-gateway --from=quay.io/redhat-service-assurance/smart-gateway:latest --confirm
+oc import-image smart-gateway-operator --from=quay.io/redhat-service-assurance/smart-gateway-operator:latest --confirm
 
 oc set image-lookup prometheus prometheus-operator prometheus-configmap-reloader prometheus-config-reloader prometheus-alertmanager amq-interconnect amq-interconnect-operator smart-gateway smart-gateway-operator
