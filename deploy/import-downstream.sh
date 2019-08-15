3cc4960f10cc3f2ec33051189d70a0123493dab4
Leif <lmadsen@redhat.com>
2019-08-15 13:43:09 -0400

    Update image versions prior to SAF release

--- deploy/import-downstream.sh
+++ deploy/import-downstream.sh
@@ -15,10 +15,10 @@ oc import-image prometheus-config-reloader:latest --from=registry.redhat.io/open
 # specific version format which effectively assumes a version of 0.15.0.
 oc import-image prometheus-alertmanager:v0.15.0 --from=registry.redhat.io/openshift3/prometheus-alertmanager:v3.11 --confirm
 
-# Version 1.4-6 here is used to reference the expected downstream version from
+# Version 1.4-7 here is used to reference the expected downstream version from
 # the template and to provide some additional context for understanding the
 # mapping of upstream to downstream versions of the qdrouterd container image.
-oc import-image amq-interconnect:1.4-6 --from=registry.redhat.io/amq7/amq-interconnect:1.4-6 --confirm
+oc import-image amq-interconnect:1.4-7 --from=registry.redhat.io/amq7/amq-interconnect:1.4-7 --confirm
 
 # Use of amq-interconnect-operator is within a tech preview space and is not
 # eligible for support, and thus is not listed on RHCC. Version for this
@@ -28,6 +28,6 @@ oc import-image amq-interconnect-operator:latest --from=registry.redhat.io/amq7-
 # Currently we don't have a robust release process for the Smart Gateway or
 # corresponding Operator, so we just pull the latest version down for now.
 oc import-image smart-gateway:latest --from=registry.redhat.io/saf/smart-gateway:1.0-3 --confirm
-oc import-image smart-gateway-operator:latest --from=registry.redhat.io/saf/smart-gateway-operator:1.0-4 --confirm
+oc import-image smart-gateway-operator:latest --from=registry.redhat.io/saf/smart-gateway-operator:1.0.4-3 --confirm
 
 oc set image-lookup prometheus prometheus-operator prometheus-configmap-reloader prometheus-config-reloader prometheus-alertmanager amq-interconnect amq-interconnect-operator smart-gateway smart-gateway-operator
