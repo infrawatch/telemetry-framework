48b12bf2d6e321efccf7de7888b377cd5fd48aba
Chris Sibbitt <csibbitt@redhat.com>
2019-10-09 16:01:03 -0400

    Scaling up the performance tests (#76)
    * Scaling up the tests
    
    * Added ability to launch multiple telemetry-bench pods
    * Fixed some minor oops' in the collectd-tg job
    * Fixed the labels on both jobs
    * Documentation merge/updates
    * Major script usage and notes update
    * New dashboard image
    * Tweaks to the dashboard
    * Fix to minishift sysctl command order
    
    * Perftest README.md edits
    
    * Update performance-test.sh

--- tests/performance-test/deploy/performance-test-tb.sh
+++ tests/performance-test/deploy/performance-test-tb.sh
@@ -4,11 +4,16 @@ COUNT=${COUNT:-180}
 HOSTS=${HOSTS:-20}
 PLUGINS=${PLUGINS:-1000}
 INTERVAL=${INTERVAL:-1}
+CONCURRENT=${CONCURRENT:-1}
 
-oc delete job/saf-performance-test || true
+oc delete job -l app=saf-performance-test || true
 
-oc create -f <(sed  -e "s/<<COUNT>>/${COUNT}/;
-                        s/<<HOSTS>>/${HOSTS}/;
-                        s/<<PLUGINS>>/${PLUGINS}/;
-                        s/<<INTERVAL>>/${INTERVAL}/"\
-                    performance-test-job-tb.yml.template)
+for i in $(seq 1 ${CONCURRENT}); do
+
+  oc create -f <(sed  -e "s/<<PREFIX>>/saf-perftest-${i}-/g;
+                          s/<<COUNT>>/${COUNT}/g;
+                          s/<<HOSTS>>/${HOSTS}/g;
+                          s/<<PLUGINS>>/${PLUGINS}/g;
+                          s/<<INTERVAL>>/${INTERVAL}/g"\
+                          performance-test-job-tb.yml.template)
+done
--- tests/performance-test/performance-test.sh
+++ tests/performance-test/performance-test.sh
@@ -10,17 +10,34 @@ Requires:
   * oc tools pointing at your SAF instance
   * gnu sed
 
-Usage: ./performance-test.sh -t <tg|tb> -c <intervals> -h <#hosts> -p <#plugins> -i <seconds>
-  -t: Which tool to use ('tg' = collectd-tg, 'tb' = telemetry-bench)
+Usage: ./performance-test.sh -t <tg|tb> -c <intervals> -h <#hosts> -p <#plugins> -i <seconds> [-n <#concurrent>]
+  -t: Which tool to use ('tg' = collectd-tg, 'tb' = telemetry-bench (recommended))
   -c: The number of intervals to run for
   -h: The number of hosts to simulate per batch
   -p: The nuber of plugins to simulate per batch
   -i: The (target) interval over which a message batch is sent
+  -n: The number of concurrent batches to run (telemetry-bench only)
+
+NOTES:
+  * The expected message throughput is roughly: <#hosts> * <#plugins> * <#concurrent> per <interval>
+  * The tools themselves are known to top out around ~18k/s (tb) and ~28k/s (tg) on modern CPUs
+  * The best way to run this at scale is with batches of 5k or 10k and a concurrency setting to acheive the desired
+    throughput
+  * telemetry-bench is recommended since there are problems getting collectd-tg to scale concurrently
+  * telemetry-bench somewhat underperforms (runs too slow), but every message does get sent
+  * A plugin setting of 1000 reasonably matches the plugins/host we expect to see from OSP
+
+EXAMPLES:
+  Quick minimal test ~1k/s (1 min)
+  ./performance-test.sh -t tb -c 60 -h 1 -p 1000 -i 1 -n 1
+
+  Recommended command for ~20k/s (10 mins)
+  ./performance-test.sh -t tb -c 600 -h 5 -p 1000 -i 1 -n 4
 ENDUSAGE
     exit 1
 }
 
-while getopts t:c:h:p:i: option
+while getopts t:c:h:p:i:n: option
 do
     case "${option}"
     in
@@ -29,6 +46,7 @@ do
         h) HOSTS=${OPTARG};;
         p) PLUGINS=${OPTARG};;
         i) INTERVAL=${OPTARG};;
+        n) CONCURRENT=${OPTARG};;
         *) ;;
     esac
 done
@@ -52,7 +70,7 @@ if [ "${TOOL}" = "tg" ]; then
     ./performance-test-tg.sh
 
 elif [ "${TOOL}" = "tb" ]; then
-    export COUNT HOSTS PLUGINS INTERVAL
+    export COUNT HOSTS PLUGINS INTERVAL CONCURRENT
     cd deploy
     ./performance-test-tb.sh
 
