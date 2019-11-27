d1b2591c73492d7c9a8d1b18996b5b8f1b92e924
Chris Sibbitt <csibbitt@redhat.com>
2019-11-27 11:08:04 -0500

    Use OCP4 OLM instead of ansible for deployment (#102)
    * Use OCP4 OLM instead of ansible for deployment
    
    * Removes all the outdated deployment machinery
    * Proposes a (very POC) replacement quickstart.sh
    
    * Update quickstart.sh
    
    * Marginally nicer wait-state messages in quickstart

--- deploy/quickstart.sh
+++ deploy/quickstart.sh
@@ -1,72 +1,69 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
 
-#
-# Usage:
-#
-#  Upstream containers: ./quickstart.sh
-#  Downstream containers: ./quickstart.sh --downstream-secret=~/6340056-cloudops-pull-secret.yaml
-oc login -u system:admin
-if oc get project sa-telemetry; then 
-    echo "Utilizing existing project 'sa-telemetry'"
-    oc project sa-telemetry
-else
-    echo "Creating new project 'sa-telemetry'"
-    oc new-project sa-telemetry    
-fi
+SAF_PROJECT=${SAF_PROJECT:-sa-telemetry}
+oc new-project "${SAF_PROJECT}"
+oc create -f - <<EOF
+apiVersion: operators.coreos.com/v1
+kind: OperatorSource
+metadata:
+  name: redhat-service-assurance-operators
+  namespace: openshift-marketplace
+spec:
+  type: appregistry
+  endpoint: https://quay.io/cnr
+  registryNamespace: redhat-service-assurance
+  displayName: Service Assurance Operators
+  publisher: Red Hat (CloudOps)
 
-# make sure there is a node that matches ElasticSearch's node selector
-oc patch node $(oc get node | tail -1 | awk '{print $1}') -p '{"metadata":{"labels":{"kubernetes.io/os": "linux"}}}'
+---
 
-# generate certificates for AMQ Interconnect
-openssl req -new -x509 -batch -nodes -days 11000 \
-        -subj "/O=io.interconnectedcloud/CN=qdr-white.sa-telemetry.svc.cluster.local" \
-        -out qdr-server-certs/tls.crt \
-        -keyout qdr-server-certs/tls.key
-oc create secret tls qdr-white-cert \
-    --cert=qdr-server-certs/tls.crt \
-    --key=qdr-server-certs/tls.key \
-    --dry-run=true -o yaml | oc apply -f -
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: amq7-cert-manager
+  namespace: openshift-operators
+spec:
+  channel: alpha
+  installPlanApproval: Automatic
+  name: amq7-cert-manager
+  source: redhat-operators
+  sourceNamespace: openshift-marketplace
+  startingCSV: amq7-cert-manager.v1.0.0
 
-# generate certificates for ElasticSearch
-WORKING_DIR=./es-certs NAMESPACE="sa-telemetry" ./cert_generation.sh
-cd es-certs
+---
 
-# ref: https://github.com/openshift/cluster-logging-operator/blob/master/pkg/k8shandler/logstore.go#L76-L90
-oc create secret generic elasticsearch \
-    --from-file=elasticsearch.key \
-    --from-file=elasticsearch.crt \
-    --from-file=logging-es.key \
-    --from-file=logging-es.crt \
-    --from-file=admin-key=system.admin.key \
-    --from-file=admin-cert=system.admin.crt \
-    --from-file=admin-ca=ca.crt \
-    --dry-run=true -o yaml | oc apply -f -
+apiVersion: operators.coreos.com/v1alpha2
+kind: OperatorGroup
+metadata:
+  name: ${SAF_PROJECT}-og
+  namespace: ${SAF_PROJECT}
+spec:
+  targetNamespaces:
+  -  ${SAF_PROJECT}
 
-cd ..
+---
 
-# build out manifests
-ansible-playbook \
-    -e "registry_path=$(oc registry info)" \
-    -e "imagestream_namespace=$(oc project --short)" \
-    -e "prometheus_pvc_storage_request=2G" \
-    deploy_builder.yml
-
-# import container images
-if [[ "$*" =~ --downstream-secret=(.*) ]]; then
-    echo "Importing containers from downstream"
-    eval secret_file="${BASH_REMATCH[1]}"  # eval is for ~ expansion
-    # shellcheck disable=SC2154
-    oc create -f "${secret_file}"
-    ./import-downstream.sh
-else
-    echo "Importing containers from upstream"
-    ./import-upstream.sh
-fi
-
-# deploy the environment (requires interaction)
-./deploy.sh
-
-# teardown the environment when done (requires interaction)
-echo "Your environment should now be ready."
-echo "-- To teardown your environment, run:   ./deploy.sh DELETE"
+apiVersion: operators.coreos.com/v1alpha1
+kind: Subscription
+metadata:
+  name: service-assurance-operator
+  namespace: ${SAF_PROJECT}
+spec:
+  channel: alpha
+  installPlanApproval: Automatic
+  name: service-assurance-operator
+  source: redhat-service-assurance-operators
+  sourceNamespace: openshift-marketplace
+  startingCSV: service-assurance-operator.v0.1.0
+EOF
+while ! oc get csv | grep service-assurance-operator | grep Succeeded; do echo "waiting for SAO..."; sleep 3; done
+oc create -f - <<EOF
+apiVersion: infra.watch/v1alpha1
+kind: ServiceAssurance
+metadata:
+  name: saf-default
+  namespace: ${SAF_PROJECT}
+spec:
+  metricsEnabled: true
+  eventsEnabled: true
+EOF
