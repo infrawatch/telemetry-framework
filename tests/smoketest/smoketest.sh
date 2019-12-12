2732b834f1f33175d1951065879514f36a9273a5
Matthias Runge <mrunge@redhat.com>
2019-12-12 16:36:05 +0100

    Adjust connection between sg and collectd

--- tests/smoketest/smoketest.sh
+++ tests/smoketest/smoketest.sh
@@ -86,13 +86,11 @@ oc logs "$(oc get pod -l application=saf-default-interconnect -o jsonpath='{.ite
 echo
 
 echo "*** [INFO] Logs from smart gateways..."
-for NAME in "${CLOUDNAMES[@]}"; do
-    oc logs "$(oc get pod -l "deploymentconfig=${NAME}-smartgateway" -o jsonpath='{.items[0].metadata.name}')"
-done
+oc logs "$(oc get pod -l "deploymentconfig=saf-default-telemetry-smartgateway" -o jsonpath='{.items[0].metadata.name}')"
 echo
 
 echo "*** [INFO] Logs from smart gateway operator..."
-oc logs "$(oc get pod -l app=smart-gateway-operator -o jsonpath='{.items[0].metadata.name}')"
+oc logs "$(oc get pod -l app=smart-gateway -o jsonpath='{.items[0].metadata.name}')" -c operator
 echo
 
 echo "*** [INFO] Logs from prometheus..."
